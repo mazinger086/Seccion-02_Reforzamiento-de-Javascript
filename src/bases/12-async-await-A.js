872c12f7038cd70fcb4e46f308458070d60d21e8
Daniel Lasauskas <Mazinger_086@yahoo.com>
2023-04-05 21:55:52 -0300

    12-async-await-A finalizado

---
+++ src/bases/12-async-await-A.js
@@ -0,0 +1,18 @@
+
+
+console.log('Hola mundo!!');
+
+
+
+
+
+
+
+
+
+
+// import { heroesByID, getHeroesByOwner } from './bases/08-imp-exp';
+
+// console.log(heroesByID(2));
+
+// console.log(getHeroesByOwner('Marvel'));
--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
-import axios from 'axios';
+
+const miPromesa = () => {
+    return new Promise( (resolve, reject) => {
+        setTimeout(() => {
+            // resolve('Tenemos un valor en la promesa')
+            reject('Tenemos un error en el valor de la promesa')
+        }, 1000)
+    })
+}
+
+
+const medirTiempoAsync = async() => {
+    try {
+        console.log('Inicio');
+
+        const respuesta = await miPromesa()
+        console.log(respuesta);
+
+        console.log('Fin'); 
+    } catch (error) {
+        // return 'Catch en medir tiempo Async' 
+        throw 'Error en medirTiempoAsync'
+    }
+
+   
+}
 
 
 
-console.log('Hola Mundo!!');
 
-
-
-
-
-
-
+medirTiempoAsync()
+    .then(valor => console.log(valor)) // Con los return retornamos valores esperados en el .then
+    .catch(err => console.log(err)) // en este caso lo catchea por el throw por que es un error
+    
 
 
 
