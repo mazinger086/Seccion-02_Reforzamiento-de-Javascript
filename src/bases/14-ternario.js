17886d03a1d12c94b8912ea3da975312e285ae76
Daniel Lasauskas <Mazinger_086@yahoo.com>
2023-04-06 09:37:59 -0300

    14-ternario finalizado

---
+++ src/bases/14-ternario.js
@@ -0,0 +1,13 @@
+
+
+let firstName;
+let lastName = 'Herrera';
+
+
+// console.log(`${ firstName || 'No firstName' } ${ lastName || 'No lastName' }`);
+
+const isActive = false
+
+const msj = isActive ? 'Esta activo' : 'No esta activo'
+
+console.log(msj);
--- src/index.js
+++ src/index.js
@@ -1,23 +1,2 @@
-import giphyApi from "./bases/11-axios";
 
-
-
-const getImages = async () => {
-    try {
-        
-        const { data } = (await giphyApi.get('/random')).data
-        const { url } = data.images.original
-
-        const image = document.createElement('img');
-        image.src = url
-        document.body.append(image)
-
-    } catch (error) {
-        console.log('Error en la peticion', error);
-        throw error
-    }
-}
-
-
-
-getImages()
+console.log('Hello world');
